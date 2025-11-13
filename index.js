e6572be627f19db22ba36c197896f71df479ac14
Your Name <you@example.com>
2025-11-13 20:59:07 +0100

    Checking Corse

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import authRoutes from "./routes/auth.route.js";
 
 const app = express()
 const PORT = process.env.PORT || 4000
-const allowedOrigins = [process.env.CLIENT_URL || "http://localhost:5173"]
+const allowedOrigins = ["https://auth-frontend-8k2b303ha-ndrss-projects.vercel.app", "http://localhost:5173"]
 app.use(cors({
     origin: allowedOrigins,
     credentials: true,
